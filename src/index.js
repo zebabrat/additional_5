4c13916836d229acbe89710c9dd163abd4b22885
Андрей Забавко <zeba.brat@Zebabrat.local>
2018-09-23 22:56:51 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) { return false }
+  let arrBracket = str.split('');
+  let stack = [];
+  
+  for (let i = 0; i < arrBracket.length; i++) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      ( stack[stack.length - 1] === bracketsConfig[k][0] && arrBracket[i] === bracketsConfig[k][1] ) ? 
+      stack.pop() : 
+      ( arrBracket[i] === bracketsConfig[k][0] ? stack.push(arrBracket[i] ) : '');
+    }
+  }
+ return  (stack.length === 0 ? true : false)
 }
